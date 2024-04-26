9e8312c0404488cadf0b6c2e43be32aabe0cb9d0
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-27 01:59:32 +0300

    commit_add_billingDto

---
+++ Frontends/KMNVegetable.Dto/BillingDtos/CreateBillingDto.cs
@@ -0,0 +1,23 @@
+﻿namespace KMNVegetable.Dto.BillingDtos
+{
+    public class CreateBillingDto
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string? Annotation { get; set; }
+    }
+}
---
+++ Frontends/KMNVegetable.Dto/BillingDtos/ResultBillingDto.cs
@@ -0,0 +1,25 @@
+﻿namespace KMNVegetable.Dto.BillingDtos
+{
+    public class ResultBillingDto
+    {
+        public int BillingID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Postcode { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string? Annotation { get; set; }
+    }
+}
