5d4135e0ba0afb4c5298bf70a167a4cb8f10fec7
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-27 02:21:46 +0300

    commit_add_testimonialDto

---
+++ Frontends/KMNVegetable.Dto/TestimonialDtos/CreateTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KMNVegetable.Dto.TestimonialDtos
+{
+    public class CreateTestimonialDto
+    {
+        public string TestimonialName { get; set; }
+
+        public string TestimonialDescription { get; set; }
+
+        public string TestimonialRating { get; set; }
+    }
+}
---
+++ Frontends/KMNVegetable.Dto/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KMNVegetable.Dto.TestimonialDtos
+{
+    public class ResultTestimonialDto
+    {
+        public int TestimonialID { get; set; }
+
+        public string TestimonialName { get; set; }
+
+        public string TestimonialDescription { get; set; }
+
+        public string TestimonialRating { get; set; }
+    }
+}
