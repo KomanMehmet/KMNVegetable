ca776ba266a3e299a10f538a9813b9a987de085f
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-27 01:50:07 +0300

    commit_add_adminLayout

---
+++ Frontends/KMNVegetable.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KMNVegetable.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
