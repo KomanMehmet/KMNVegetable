c5c7598c95094aa92d9ab5481b801c09ec651c95
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-27 02:16:04 +0300

    commit_add_promotionDto

---
+++ Frontends/KMNVegetable.Dto/PromotionDtos/GetByIdPromotionDto.cs
@@ -0,0 +1,18 @@
+﻿
+namespace KMNVegetable.Dto.PromotionDtos
+{
+    public class GetByIdPromotionDto
+    {
+        public int PromotionID { get; set; }
+
+        public string PromotionTitle1 { get; set; }
+
+        public string PromotionTitle2 { get; set; }
+
+        public string PromotionDescripiton { get; set; }
+
+        public string PromotionImageUrl { get; set; }
+
+        public decimal PromotionPrice { get; set; }
+    }
+}
---
+++ Frontends/KMNVegetable.Dto/PromotionDtos/ResultPromotionDto.cs
@@ -0,0 +1,17 @@
+﻿namespace KMNVegetable.Dto.PromotionDtos
+{
+    public class ResultPromotionDto
+    {
+        public int PromotionID { get; set; }
+
+        public string PromotionTitle1 { get; set; }
+
+        public string PromotionTitle2 { get; set; }
+
+        public string PromotionDescripiton { get; set; }
+
+        public string PromotionImageUrl { get; set; }
+
+        public decimal PromotionPrice { get; set; }
+    }
+}
