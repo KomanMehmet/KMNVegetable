e9f4855110588ec365d43815f7a0a4f4c3044fb5
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-27 02:14:04 +0300

    commit_add_productdetailDto

---
+++ Frontends/KMNVegetable.Dto/ProductDetailDtos/UpdateProductDetailDto.cs
@@ -0,0 +1,27 @@
+﻿namespace KMNVegetable.Dto.ProductDetailDtos
+{
+    public class UpdateProductDetailDto
+    {
+        public int ProductDetailID { get; set; }
+
+        public string ProductDetailName { get; set; }
+
+        public int ProductID { get; set; }
+
+        public decimal ProductPrice { get; set; }
+
+        public int ProductRating { get; set; }
+
+        public string ProductDescription { get; set; }
+
+        public string ProductWeight { get; set; }
+
+        public string ProductOrigin { get; set; }
+
+        public string ProductQuality { get; set; }
+
+        public string ProductCheck { get; set; }
+
+        public string ProductMinWeight { get; set; }
+    }
+}
