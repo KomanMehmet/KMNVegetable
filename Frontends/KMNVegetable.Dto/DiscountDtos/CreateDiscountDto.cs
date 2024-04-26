f55dc6299a43dc3ed5f4bd1d3b849ff25906b8ed
KomanMehmet <108063848+KomanMehmet@users.noreply.github.com>
2024-04-27 02:05:06 +0300

    commit_add_discountDto

---
+++ Frontends/KMNVegetable.Dto/DiscountDtos/CreateDiscountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KMNVegetable.Dto.DiscountDtos
+{
+    public class CreateDiscountDto
+    {
+        public string DiscoundProductName { get; set; }
+
+        public string DiscoundProductAmount { get; set; }
+
+        public int ProductID { get; set; }
+    }
+}
---
+++ Frontends/KMNVegetable.Dto/DiscountDtos/GetByIdCommentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KMNVegetable.Dto.DiscountDtos
+{
+    public class GetByIdCommentDto
+    {
+        public int DiscountID { get; set; }
+
+        public string DiscoundProductName { get; set; }
+
+        public string DiscoundProductAmount { get; set; }
+
+        public int ProductID { get; set; }
+    }
+}
